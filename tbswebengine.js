79aa82c34e6f25557cb61dceb1e0dff3e2a95642
teguhbs7 <69722686+teguhbs7@users.noreply.github.com>
2020-09-30 01:01:48 +0800

    Update tbswebengine.js

--- tbswebengine.js
+++ tbswebengine.js
@@ -1,47 +1,3 @@
-// Navbar
-const nav=document.querySelector(".nav-tbs"),
-      navList=nav.querySelectorAll("li"),
-      totalNavList=navList.length,
-      allSection=document.querySelectorAll(".sections"),
-      totalSection=allSection.length;
-
-for(let i=0; i<totalNavList; i++){
-const a=navList[i].querySelector("a");
-a.addEventListener("click", function(){
-  // remove back-section
-  removeBackSectionClass();
-
-  for(let j=0; j<totalNavList; j++){
-    if(navList[j].querySelector("a").classList.contains("active")){
-        // add back section class
-        addBackSectionClass(j)
-  }
-  navList[j].querySelector("a").classList.remove("active");
-}
-this.classList.add("active");
-showSection(this);
-})
-}
-
-function showSection(element){
-  for(let i=0; i<totalSection; i++){
-    allSection[i].classList.remove("active");
-  }
-  
-    const target=element.getAttribute("href").split("#")[1];
-  document.querySelector("#"+target).classList.add("active")
-
-}
-function addBackSectionClass(num){
-  allSection[num].classList.add("back-section");
-}
-function removeBackSectionClass(){
-  for(let i=0; i<totalSection; i++){
-      allSection[i].classList.remove("back-section");
-  }
-}
-
-
 // Portfolio Item Filter
 
 const filterContainer=document.querySelector(".portfolio-filter"),
@@ -129,3 +85,49 @@ const filterContainer=document.querySelector(".portfolio-filter"),
 
     })
 
+
+
+
+
+// Navbar
+const nav=document.querySelector(".nav-tbs"),
+      navList=nav.querySelectorAll("li"),
+      totalNavList=navList.length,
+      allSection=document.querySelectorAll(".sections"),
+      totalSection=allSection.length;
+
+for(let i=0; i<totalNavList; i++){
+const a=navList[i].querySelector("a");
+a.addEventListener("click", function(){
+  // remove back-section
+  removeBackSectionClass();
+
+  for(let j=0; j<totalNavList; j++){
+    if(navList[j].querySelector("a").classList.contains("active")){
+        // add back section class
+        addBackSectionClass(j)
+  }
+  navList[j].querySelector("a").classList.remove("active");
+}
+this.classList.add("active");
+showSection(this);
+})
+}
+
+function showSection(element){
+  for(let i=0; i<totalSection; i++){
+    allSection[i].classList.remove("active");
+  }
+  
+    const target=element.getAttribute("href").split("#")[1];
+  document.querySelector("#"+target).classList.add("active")
+
+}
+function addBackSectionClass(num){
+  allSection[num].classList.add("back-section");
+}
+function removeBackSectionClass(){
+  for(let i=0; i<totalSection; i++){
+      allSection[i].classList.remove("back-section");
+  }
+}
