5fb53a71ce338412e8c3ee021106085c35773471
teguhbs7 <69722686+teguhbs7@users.noreply.github.com>
2020-10-26 13:27:00 +0800

    Update loginsystemFS-tbs.js

--- loginsystemFS-tbs.js
+++ loginsystemFS-tbs.js
@@ -1,91 +1,22 @@
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("login-form-submit");
+const loginErrorMsg = document.getElementById("login-error-msg");
 
-  var objPeople = [
-	{ // Object @ 0 index - AJP
-		username: "imron",
-		password: "imron07"
-	},
-	{ // Object @ 1 index - BUMA LATI
-		username: "faisal",
-		password: "hfaisal998"
-	},
-	{ // Object @ 2 index
-		username: "kurnia",
-		password: "kurniacr820"
-	},
-	{ // Object @ 2 index
-		username: "susanto",
-		password: "ssanto468"
-	},
-	{ // Object @ 2 index
-		username: "nur",
-		password: "nurtri553"
-	},
-	{ // Object @ 2 index
-		username: "wawan",
-		password: "wawans73"
-	},
-	{ // Object @ 2 index
-		username: "rachmad",
-		password: "rachmad57"
-	},
-	{ // Object @ 2 index
-		username: "ari",
-		password: "ari735"
-	},
-	{ // Object @ 2 index
-		username: "indra",
-		password: "indrabudikusuma"
-	}
-	  
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const username = loginForm.username.value;
+    const password = loginForm.password.value;
 
-]
-
-function getInfo() {
-	var username = document.getElementById('username').value
-	var password = document.getElementById('password').value
-
-	for(var i = 0; i < objPeople.length; i++) {
-		// check is user input matches username and password of a current index of the  objPeople array
-
-//		if(username == objPeople[i].username && password == objPeople [i].password) {
-//            function btntest_onclick(){
-//            	window.location.href = "https://support-fs.blogspot.com/p/controls.html";
-//            } btntest_onclick()
-//          return;
-//    	}
-		
-		if(username == imron && password == imron07) {
-            function btntest_onclick(){
-            	window.location.href = "https://support-fs.blogspot.com/p/controls.html";
-            } btntest_onclick()
-          return;
-    	}
-      
-      
-		if(username == faisal && password == hfaisal998) {
-           function btntest_onclick(){
-            	window.location.href = "http://google.com/";
-           } btntest_onclick()
-          return;
-        }
-      
-      		if(username == kurnia && password == kurniacr820) {
-           function btntest_onclick(){
-            	window.location.href = "http://google.com/";
-           } btntest_onclick()
-          return;
-        }
-      	
-      		if(username == susanto && password == ssanto468) {
-           function btntest_onclick(){
-            	window.location.href = "http://google.com/";
-           } btntest_onclick()
-          return;
-        } else{
-		document.getElementById("alerts3").style.display = "block";
-             	return;
-	} btntest_onclick();
-      
-	}
-		//console.log("incorrect username or password")
-}
+    if (username === "imron" && password === "imron07") {
+        /*alert("You have successfully logged in.");*/
+        window.location.href = "https://support-fs.blogspot.com/p/controls.html";
+        return;
+    } else if (username === "faisal" && password === "hfaisal998") {
+        /*alert("You have successfully logged in.");*/
+        window.location.href = "https://www.google.com/";
+        /*location.reload();*/
+        return;
+    } else {
+        loginErrorMsg.style.opacity = 1;
+    }
+})
