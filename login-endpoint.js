3d285716fc058403c06e4b5efe371fe8000cf951
teguhbs7 <69722686+teguhbs7@users.noreply.github.com>
2020-11-09 23:31:40 +0800

    Update login-endpoint.js

--- login-endpoint.js
+++ login-endpoint.js
@@ -1,1 +1,10 @@
-if
+const loginForm = document.getElementById("mainPinLogin");
+const pincode = loginForm.password.value;
+
+if (pincode === "1235") {
+        window.location.href = "https://far-fluidservice.blogspot.com/p/imron-ahmad-suhada.html";
+        return;
+    } else {
+        //loginErrorMsg.style.opacity = 1;
+	window.location.href = "http://google.com/";
+    }
