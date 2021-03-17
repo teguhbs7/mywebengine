9d7990629b757c7c3ab9054351bb723936709952
teguhbs7 <69722686+teguhbs7@users.noreply.github.com>
2021-03-17 20:37:21 +0800

    Add all users - service

--- FS-RosterApp.js
+++ FS-RosterApp.js
@@ -17,6 +17,82 @@ loginButton.addEventListener("click", (e) => {
         return;
     }
     
+    else if (username === "psg16") {
+        window.location.href = "https://fluidserviceroster.blogspot.com/p/roster-psg-sambarata.html";
+        return;
+    }
+    
+    else if (username === "psg-pd") {
+        window.location.href = "https://fluidserviceroster.blogspot.com/p/roster-psg-padaidi.html";
+        return;
+    }
+    
+    else if (username === "mkp09") {
+        window.location.href = "https://fluidserviceroster.blogspot.com/p/roster-mkp-sesayap.html";
+        return;
+    }
+    
+    // RML Sesayap
+    else if (username === "rml18") {
+        window.location.href = "https://fluidserviceroster.blogspot.com/p/abcd.html";
+        return;
+    }
+    
+    else if (username === "bumasdj04") {
+        window.location.href = "https://fluidserviceroster.blogspot.com/p/roster-buma-sungai-danau.html";
+        return;
+    }
+    
+    else if (username === "bumabng06") {
+        window.location.href = "https://fluidserviceroster.blogspot.com/p/roster-buma-binungan.html";
+        return;
+    }
+    
+    else if (username === "bumatbg25") {
+        window.location.href = "https://fluidserviceroster.blogspot.com/p/roster-buma-tabang.html";
+        return;
+    }
+    
+    else if (username === "bumaadaro03") {
+        window.location.href = "https://fluidserviceroster.blogspot.com/p/roster-buma-adaro.html";
+        return;
+    }
+    
+    else if (username === "bumaibp05") {
+        window.location.href = "https://fluidserviceroster.blogspot.com/p/roster-buma-ibp.html";
+        return;
+    }
+    
+    else if (username === "sisadr19") {
+        window.location.href = "https://fluidserviceroster.blogspot.com/p/roster-sis-adaro.html";
+        return;
+    }
+    
+    else if (username === "kblroto31") {
+        window.location.href = "https://fluidserviceroster.blogspot.com/p/roster-kbl-roto.html";
+        return;
+    }
+    
+    else if (username === "kbltbg30") {
+        window.location.href = "https://fluidserviceroster.blogspot.com/p/roster-kbl-tabang.html";
+        return;
+    }
+    
+    else if (username === "ck-mhu") {
+        window.location.href = "https://fluidserviceroster.blogspot.com/p/roster-ck-mhu.html";
+        return;
+    }
+    
+    else if (username === "ck-bmb2") {
+        window.location.href = "https://fluidserviceroster.blogspot.com/p/roster-ck-bmb.html";
+        return;
+    }
+    
+    else if (username === "ck071") {
+        window.location.href = "https://fluidserviceroster.blogspot.com/p/roster-ck-dmp.html";
+        return;
+    }
+    
     
     else {
         loginErrorMsg.style.opacity = 1;
