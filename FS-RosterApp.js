e6c4d1e35d1428f54f6c4c8b1ac9f4972f6a8b18
teguhbs7 <69722686+teguhbs7@users.noreply.github.com>
2021-06-24 14:14:08 +0800

    Update FS-RosterApp.js

--- FS-RosterApp.js
+++ FS-RosterApp.js
@@ -5,7 +5,7 @@ const loginErrorMsg = document.getElementById("login-error-msg");
 loginButton.addEventListener("click", (e) => {
     e.preventDefault();
     const username = loginForm.username.value;
-    const username = loginForm.section.value;
+    const section = loginForm.section.value;
     
     
     
