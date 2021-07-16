a4f9a0e236921e522c0ee4d1f787fa4c16f40d39
teguhbs7 <69722686+teguhbs7@users.noreply.github.com>
2021-07-16 13:45:41 +0800

    Update FSSales-Login.js

--- FSSales-Login.js
+++ FSSales-Login.js
@@ -15,7 +15,7 @@ loginButton.addEventListener("click", (e) => {
        (username === "Arsy12nisa" && site === "buma-adaro") || 
        (username === "moedianto" && site === "buma-adaro") || 
        (username === "renita1906" && site === "buma-adaro") || 
-        (username === "wahyu497" && site === "buma-adaro") || ) 
+       (username === "wahyu497" && site === "buma-adaro") ||
        (username === "teguhbs7" && site === "buma-adaro") || ) {
         window.location.href = "https://fs-sales.blogspot.com/p/my-fs-adaro-sis.html";
         return;
@@ -29,20 +29,22 @@ loginButton.addEventListener("click", (e) => {
        (username === "Arsy12nisa" && site === "sis-adaro") || 
        (username === "moedianto" && site === "sis-adaro") || 
        (username === "renita1906" && site === "sis-adaro") || 
-        (username === "wahyu497" && site === "sis-adaro") || ) 
+        (username === "wahyu497" && site === "sis-adaro") ||
        (username === "teguhbs7" && site === "sis-adaro") || ) {
         window.location.href = "https://fs-sales.blogspot.com/p/my-fs-adaro-sis.html";
         return;
-    }  else if ((username === "daniel460" && site === "sis-adaro") || 
-       (username === "chrism663" && site === "buma-adaro") || 
-       (username === "veridwi985" && site === "buma-adaro") || 
-       (username === "marselkl17" && site === "buma-adaro") || 
-       (username === "Arsy12nisa" && site === "buma-adaro") || 
-       (username === "moedianto" && site === "buma-adaro") || 
-       (username === "renita1906" && site === "buma-adaro") || 
-        (username === "wahyu497" && site === "buma-adaro") || ) 
-       (username === "teguhbs7" && site === "buma-adaro") || ) {
-        window.location.href = "https://fs-sales.blogspot.com/p/my-fs-adaro-sis.html";
+        
+    // PPA Satui
+    }  else if ((username === "daniel460" && site === "ppa-satui") || 
+       (username === "chrism663" && site === "ppa-satui") || 
+       (username === "veridwi985" && site === "ppa-satui") || 
+       (username === "marselkl17" && site === "ppa-satui") || 
+       (username === "Arsy12nisa" && site === "ppa-satui") || 
+       (username === "moedianto" && site === "ppa-satui") || 
+       (username === "renita1906" && site === "ppa-satui") || 
+        (username === "wahyu497" && site === "ppa-satui") ||
+       (username === "teguhbs7" && site === "ppa-satui") || ) {
+        window.location.href = "https://fs-sales.blogspot.com/p/sales-ppa-satui.html";
         return;
     } else {
         loginErrorMsg.style.opacity = 1;
